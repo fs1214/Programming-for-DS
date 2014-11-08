f6ddf03a2debea278cc24077c5d382e5ba3b6112
ZhuoranLyu <zl953@nyu.edu>
2014-11-08 13:09:56 -0500

    aaa

--- FinalProject/FirstBlog/blog/views.py
+++ FinalProject/FirstBlog/blog/views.py
@@ -11,35 +11,8 @@ from django.template import RequestContext, loader
 import pythoncode
  
 def home(request):
-    content = {
-		'title' : 'My First Post',
-		'author' : 'lzr',
-		'date' : '11/07/14',
-		'body' : 'wqnmlgb',
-	}
-    if(request.POST.get('mybtn')):
-        print('click')		
+    	
     return render_to_response('index.html',content)
-
-def request_page(request):
-	if(request.POST.get('mybtn')):
-		print('click')
-	return render_to_response('index.html')
-
-def graph(request):
-	x = [1,2,3,4,5,6]
-	y = [5,2,6,8,2,7]
-	plot(x,y, linewidth=2)
-
-	buffer = StringIO.StringIO()
-	canvas = pylab.get_current_fig_manager().canvas
-	canvas.draw()
-	graphIMG = PIL.Image.fromstring("RGB",canvas.get_width_height(), canvas.tostring_rgb())
-	graphIMG.save(buffer,"PNG")
-	pylab.close()
-
-	return HttpResponse(buffer.getvalue(), mimetype="image/png")
-
 
 
 
