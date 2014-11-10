cbdcb3476ccf199d2c6e34d4e222c67c60c4d5b0
ZhuoranLyu <zl953@nyu.edu>
2014-11-10 09:16:40 -0500

    new web

--- FinalProject/FirstBlog/blog/views.py
+++ FinalProject/FirstBlog/blog/views.py
@@ -2,17 +2,32 @@ from django.shortcuts import render_to_response
 from matplotlib import pylab
 from pylab import * 
 from blog.models import posts
-
+import json
 import PIL
 import PIL.Image
 import StringIO
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 import pythoncode
+import pandas.io.data as web
+import datetime
  
 def home(request):
+    start = datetime.datetime(2010,1,1)
+    end = datetime.datetime(2010,1,30)
+    f = web.DataReader('IBM','yahoo',start,end)
+    a = f['Open']
+    b = a.index.tolist()
+    array = [['date', 'price']]
+    for i in range(b.__len__()):
+	c = b[i]
+    	s = str(c)[:10]
+    	d = a.tolist()
+    	e = round(d[i],2)    
+    	array.append([s,e])
+
     	
-    return render_to_response('index.html')
+    return render_to_response('index.html',{'array':json.dumps(array)})
 
 
 
