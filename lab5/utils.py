b634f3521c65f9ab62481930b2f2a5c40258f5e5
ZhuoranLyu <zl953@nyu.edu>
2014-11-11 10:10:23 -0500

    restart

--- lab5/utils.py
+++
@@ -1,7 +0,0 @@
-
-def read(fn):
-    # also look at readline() and readlines() functions
-    return open(fn,'r').read()
-
-x = {'and':'of', 'square':'root', 'center':'point', 'microscope':'image'}
-
--- lab7/foo/bar.py
+++
@@ -1,7 +0,0 @@
-a=5
-
-class Bar:
-    def __init__(self, items):
-        self.items = items[1:-1]
-    def sum(self):
-        return sum(self.items)
